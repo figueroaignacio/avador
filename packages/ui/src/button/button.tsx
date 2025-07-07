4731024d9383ed08c592ac3682f9589bc5879137
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-07-07 20:50:49 -0300

    feat: add button variants

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -1,18 +1,31 @@
 'use client';
 
-// Styles
-import styles from './button.module.css';
-
+import { cva, VariantProps } from 'class-variance-authority';
 import { ReactNode } from 'react';
 
-interface ButtonProps {
+import styles from './button.module.css';
+
+const buttonVariants = cva(styles.button, {
+  variants: {
+    variant: {
+      primary: styles.primary,
+      secondary: styles.secondary,
+      destructive: styles.destructive,
+      outline: styles.outline,
+      ghost: styles.ghost,
+      link: styles.link,
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
+});
+
+interface ButtonProps extends VariantProps<typeof buttonVariants> {
   children: ReactNode;
+  onClick?: () => void;
 }
 
-export const Button = ({ children }: ButtonProps) => {
-  return (
-    <button className={styles.button} onClick={() => alert(`Se vienen cositas`)}>
-      {children}
-    </button>
-  );
+export const Button = ({ children, variant }: ButtonProps) => {
+  return <button className={buttonVariants({ variant })}>{children}</button>;
 };
