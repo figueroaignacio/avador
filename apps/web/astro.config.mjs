ff98f2f5037a4f6eddabd72717eebbdd8d09a82f
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-07-09 16:07:06 -0300

    feat: add sanity and firts post

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import sanity from '@sanity/astro';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react(),
+    sanity({
+      projectId: 'tmgct2ia',
+      dataset: 'avador',
+      apiVersion: '2025-07-09',
+      studioBasePath: '/studio',
+      useCdn: false,
+    }),
+  ],
 });
